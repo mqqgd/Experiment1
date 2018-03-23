62f7effd3edc5aa260737f34b1b3def2450f47d1
绽玉林 <@DESKTOP-IJ06936>
2018-03-23 19:36:10 +0800

    MQQGD

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
-public class YunSuan {
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+public class Main {
 	static Random R;
+	static ArrayList<String> al;
+	static ScriptEngineManager manager;
+	static ScriptEngine se;
 	public static void main(String[] args) {
 		R=new Random();
 		Scanner sc=new Scanner(System.in);
@@ -13,12 +21,15 @@ public class YunSuan {
 		System.out.println("请输入题目数量：");
 		int all=sc.nextInt();
 		BufferedWriter bw = null;
+		al=new ArrayList<String>();
+		double result=0;
+		manager = new ScriptEngineManager();
+		se = manager.getEngineByName("js");
 		try {
 			bw = new BufferedWriter(new FileWriter("result.txt"));
 			bw.write(id+"\n");
             bw.write(all+"\n");
 		} catch (IOException e2) {
-			// TODO Auto-generated catch block
 			e2.printStackTrace();
 		}
 		for(int n=0;n<all;n++){
@@ -30,24 +41,36 @@ public class YunSuan {
 					title=title+type(R.nextInt(4)+1);
 				}
 			}
-			title=title+"=";
+			
+			try {
+				result=Double.valueOf(se.eval(title).toString());
+			} catch (NumberFormatException | ScriptException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(result<0||result>200||result%1!=0){
+				n--;
+				continue;
+			}
 			try {
 	        	bw.write(title);
+	        	bw.write("=");
+	        	bw.write(String.valueOf((int)result));
 	        	bw.write("\n");
-	            
 	        } catch (IOException e1) {
-	            // TODO Auto-generated catch block
 	            e1.printStackTrace();
 	        }
-			System.out.println(title);
-		}
+			
+		}//over
+		
+		
 		try {
 			bw.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
 	public static String type(int n){
 		if(n==1){
 			return "+";
